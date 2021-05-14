957df8fe843d43dd6dc042f04b5c4d26d23ba276
nikrachedi <nikkirachedi@gmail.com>
2021-05-13 19:02:38 -0700

    Added search engine

--- src/app.js
+++ src/app.js
@@ -44,7 +44,19 @@ function displayTemperature(repsonse) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "de34744b6884c5bfe502bec9bc0725c0";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "de34744b6884c5bfe502bec9bc0725c0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
