05c1cea2afb853e9405e789d8b5fee401f1f2dc5
nikrachedi <nikkirachedi@gmail.com>
2021-05-13 18:47:59 -0700

    Added relevant Icon from API

--- src/app.js
+++ src/app.js
@@ -28,17 +28,23 @@ function displayTemperature(repsonse) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
-  let dateElement = document.querySelector("date");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org.img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "7a59bc18278b7a3ad9416769fac5647c";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=7a59bc18278b7a3ad9416769fac5647c
-&units=metric`;
+let apiKey = "de34744b6884c5bfe502bec9bc0725c0";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
