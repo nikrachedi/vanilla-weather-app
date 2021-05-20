28f93102f277b7d0aebeca899ed70ab13fa798d8
nikrachedi <nikkirachedi@gmail.com>
2021-05-19 20:14:26 -0700

    Added Forecast into JS

--- src/app.js
+++ src/app.js
@@ -22,6 +22,36 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+                <div class="col-2">
+                  <div class="weather-forecast-date">${day}</div>
+                  <div>
+                    <img
+                      src="http://openweathermap.org/img/wn/01n@2x.png"
+                      alt=""
+                      width="42"
+                    />
+                  </div>
+                  <div class="weather-forecast-temperatures">
+                    <span class="weather-forecast-temperature-max">18° </span>
+                    <span class="weather-forecast-temperature-min">12° </span>
+                  </div>
+                </div>
+              `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -85,3 +115,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
